cc21a57be88c89aa975b7ffde61ed86524a5224d
Bashar Hamade <bhcoding69@gmail.com>
2022-09-21 22:28:27 +0200

    basic rest api

---
+++ src/main/java/com/springbootstart/Springbootstart/entity/Department.java
@@ -0,0 +1,79 @@
+package com.springbootstart.Springbootstart.entity;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+
+@Entity
+public class Department {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @PositiveOrZero
+    private Long departmentId;
+    
+    @NotBlank(message="Please add department name")
+    @Length(max = 10,min = 0)
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.departmentAddress = departmentAddress;
+        this.departmentCode = departmentCode;
+    }
+
+    public Department() {
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentAddress() {
+        return departmentAddress;
+    }
+
+    public void setDepartmentAddress(String departmentAddress) {
+        this.departmentAddress = departmentAddress;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public void setDepartmentCode(String departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentId=" + departmentId +
+                ", departmentName='" + departmentName + '\'' +
+                ", departmentAddress='" + departmentAddress + '\'' +
+                ", departmentCode='" + departmentCode + '\'' +
+                '}';
+    }
+}
